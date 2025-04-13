387f2c66e992e248697eb254a2a9a87eed7f3051
ADS-Kablan <202503557233@alunos.estacio.br>
2025-04-13 19:30:12 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
-
 int main(){
     
     int pontosTuristicos1, pontosTuristicos2;
     long int populacao1, populacao2;
-    float area1, pib1, area2, pib2;
+    float area1, pib1, area2, pib2, densidadeDemografica1, densidadeDemografica2, pibPerCapita1, pibPerCapita2;
     char estado1, codigoCarta1[3], codigo1[3], cidade1[20], estado2, codigoCarta2[3], codigo2[3], cidade2[20];
 
     printf("Insira as informações da carta 1: \n");
@@ -22,8 +20,14 @@ int main(){
     printf("Digite a Populacao: \n");
     scanf("%d", &populacao1);
 
+    printf("Digite a área: \n");
+    scanf("%f", &area1);
+
     printf("Digite o PIB: \n");
     scanf("%f", &pib1);
+
+    densidadeDemografica1 = populacao1/area1;
+    pibPerCapita1 = pib1 / populacao1;
 
     printf("Digite o numero de pontos turisticos: \n");
     scanf("%d", &pontosTuristicos1);
@@ -48,8 +52,11 @@ int main(){
     scanf("%f", &pib2);
 
     printf("Digite o numero de pontos turisticos: \n");
-    scanf("%d", &pontosTuristicos2); 
-
+    scanf("%d", &pontosTuristicos2);
+    
+    densidadeDemografica2 = populacao2/area2;
+    pibPerCapita2 = pib2 / populacao2;
+    
     printf("\n--- Carta 1 ---\n");
     printf("Estado: %c\n", estado1);
     printf("Código da Carta: %s\n", codigoCarta1);
@@ -66,6 +73,19 @@ int main(){
     printf("PIB: %.2f\n", pib2);
     printf("Pontos Turísticos: %d\n", pontosTuristicos2);
 
+    // Comparar PIB
+
+    if(pibPerCapita1 > pibPerCapita2){
+        printf("Carta 1 %s venceu! \n", cidade1 );
+    }
+    if(pibPerCapita1 < pibPerCapita2){
+        printf("Carta 2 %s venceu! \n", cidade2 );
+    } 
+    
+    if(pibPerCapita1 == pibPerCapita2) {
+        printf("Deu empate!");
+    }
+    
     return 0;
 
 }
